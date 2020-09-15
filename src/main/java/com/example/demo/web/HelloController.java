7b699a0458e1d1246704a36db885bf75e4b6bd92
puki <kikiy0205@gmail.com>
2020-09-16 00:25:16 +0800

    JPA

--- src/main/java/com/example/demo/web/HelloController.java
+++ src/main/java/com/example/demo/web/HelloController.java
@@ -1,36 +1,13 @@
 package com.example.demo.web;
 
-import com.example.demo.domain.Book;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+
 
 @Controller
-//@RequestMapping(value = "/api")
 public  class HelloController {
-//
-//    @Autowired
-//    private Book book;
-
-    @GetMapping("/books/{id}" )
-    @ResponseBody
-    public Map<String, Object> World(@PathVariable long id) {
-        System.out.println("id"+id);
-        Map<String,Object> book = new HashMap<>();
-        book.put("name","Kiki");
-        return null;
-    }
-
-    @PostMapping("/book")
-    @ResponseBody
-    public  Object post(@RequestParam("name") String name){
-
-        return null;
-    }
-
 
     @RestController
     public static class testController {
